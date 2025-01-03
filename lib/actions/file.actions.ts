940dbb3ea40ca17952498eadc12554b1a27659b4
The Cybermaniac <contact@harrisonthecybermaniac.com.ng>
2025-01-03 04:44:08 +0100

    file actions modification
    Added methods for search and sort

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -63,7 +63,7 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (currentUser: Models.Document, types: string[], searchText: string, sort: string, limit?: number) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
@@ -71,10 +71,18 @@ const createQueries = (currentUser: Models.Document) => {
     ]),
   ];
 
+  if (types.length > 0) queries.push(Query.equal("type", types));
+  if (searchText) queries.push(Query.contains("name", searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  const [sortBy, orderBy] = sort.split("-");
+
+  queries.push(orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy));
+
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({ types = [], searchText = "", sort = "$createdAt", limit }: GetFilesProps) => {
   const { databases } = await createAdminClient();
 
   try {
@@ -82,7 +90,7 @@ export const getFiles = async () => {
 
     if (!currentUser) throw new Error("User not found");
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
